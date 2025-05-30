ad8aec2070d4d967cc0156ce7ccb39be2f140314
NamNguyen1909 <namnguyen19092004@gmail.com>
2025-05-30 23:25:26 +0700

    update link

--- bemmobile/configs/Apis.js
+++ bemmobile/configs/Apis.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Sử dụng địa chỉ IP của máy tính chạy backend
   // const BASE_URL = "http://192.168.1.8:8000/";
 
-const BASE_URL = "https://7031-171-243-49-111.ngrok-free.app/";
+const BASE_URL = "https://event-management-and-online-booking.onrender.com/";
 
 // const BASE_URL = "http://192.168.44.105:8000/";
 // const BASE_URL = "http://192.168.254.171:8000/";
