c1edfc739924ac7f5b32466b22617c02cbb97bd5
NamNguyen1909 <namnguyen19092004@gmail.com>
2025-06-06 16:33:47 +0700

    fix device token

--- bookingandmanagementapis/bem/models.py
+++ bookingandmanagementapis/bem/models.py
@@ -450,10 +450,11 @@ class EventTrendingLog(models.Model):
 
 class DeviceToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, unique=True)
+    token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
         indexes = [
             models.Index(fields=['user', 'token']),
         ]
+        unique_together = ('user', 'token')
